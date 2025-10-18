a613bc0b612f3691aed5dad3cd66161cc8bb8457
Emmanuel Lijo <emmanuellijo670@gmail.com>
2025-10-18 10:33:00 +0530

    Refactor Database class to use list for users

--- app/utils/database.py
+++ app/utils/database.py
@@ -1,34 +1,38 @@
-from typing import Dict, Optional
-
 class Database:
     def __init__(self):
-        self.users = {}
+        self.users = []
         self.sessions = {}
         self.next_id = 1
 
-    def get_user_by_email(self, email: str) -> Optional[Dict]:
-        for user in self.users.values():
+    def get_user_by_email(self, email: str):
+        for user in self.users:
             if user["email"] == email:
                 return user
         return None
+
     def create_user(self, email: str, hashed_password: str = None, **kwargs):
         user = {
-         "id": self.next_id,
-         "email": email,
-         "hashed_password": hashed_password,
-         "name": kwargs.get('name'),
-         "google_id": kwargs.get('google_id'),
-         "provider": "google" if kwargs.get('google_id') else "email"
+            "id": self.next_id,
+            "email": email,
+            "hashed_password": hashed_password,
+            "name": kwargs.get('name'),
+            "google_id": kwargs.get('google_id'),
+            "provider": "google" if kwargs.get('google_id') else "email"
         }
         self.users.append(user)
         self.next_id += 1
         return user
-    def get_user_by_id(self, user_id: int) -> Optional[Dict]:
-        return self.users.get(user_id)
+
+    def get_user_by_id(self, user_id: int):
+        for user in self.users:
+            if user["id"] == user_id:
+                return user
+        return None
 
     def get_user_by_google_id(self, google_id: str):
-        return next((user for user in self.users if user.get("google_id") == google_id), None)
+        for user in self.users:  # ← And this one
+            if user.get("google_id") == google_id:
+                return user
+        return None
 
 db = Database()
-
-
