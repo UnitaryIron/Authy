411023a79cf5d62675cba131be7cbc05c1f36a66
Emmanuel Lijo <emmanuellijo670@gmail.com>
2025-10-18 08:57:53 +0530

    Add Google OAuth router and load environment variables

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ from fastapi import FastAPI
 from app.routes.auth import router as auth_router
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
+from app.routes.google_oauth import router as google_router
+from dotenv import load_dotenv
 from fastapi.middleware.cors import CORSMiddleware
 from slowapi.errors import RateLimitExceeded
 
+load_dotenv() 
 app = FastAPI(title="Logify API", description="Simple Authentication System")
 
 app.include_router(auth_router, prefix="/auth", tags=["authentication"])
+app.include_router(google_router, tags=["google-oauth"])
 
 limiter = Limiter(key_func=get_remote_address)
 app.state.limiter = limiter
@@ -37,6 +41,3 @@ if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run(app, host="0.0.0.0", port=8000)
-
-
-
