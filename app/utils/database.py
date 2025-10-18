e9955c9647a91dd8f9158ebd9e5d3ec63431a2e3
Emmanuel Lijo <emmanuellijo670@gmail.com>
2025-10-18 10:24:02 +0530

    Refactor Database class to support additional user fields

--- app/utils/database.py
+++ app/utils/database.py
@@ -6,17 +6,18 @@ class Database:
         self.sessions = {}
         self.next_id = 1
 
-    def create_user(self, email: str, hashed_password: str) -> Dict:
-        user_id = self.next_id
-        user = {
-            "id": user_id,
-            "email": email,
-            "hashed_password": hashed_password,
-            "created_at": "2025-10-18" 
-        }
-        self.users[user_id] = user
-        self.next_id += 1
-        return user
+    def create_user(self, email: str, hashed_password: str = None, **kwargs):
+    user = {
+        "id": self.next_id,
+        "email": email,
+        "hashed_password": hashed_password,
+        "name": kwargs.get('name'),
+        "google_id": kwargs.get('google_id'),
+        "provider": "google" if kwargs.get('google_id') else "email"
+    }
+    self.users.append(user)
+    self.next_id += 1
+    return user
 
     def get_user_by_email(self, email: str) -> Optional[Dict]:
         for user in self.users.values():
@@ -31,3 +32,4 @@ class Database:
         return next((user for user in self.users if user.get("google_id") == google_id), None)
 
 db = Database()
+
